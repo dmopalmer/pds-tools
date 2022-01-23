250ffe10b27e0da195c6247a48388b42b3521ed0
David Palmer <dmopalmer@gmail.com>
2022-01-22 21:15:28 -0700

    Added version 0.1 info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pds-tools',
-    version='',
+    version='0.1',
     packages=[''],
     requires=['numpy', 'pyparsing'],
     url='https://github.com/SETI/pds-tools',
